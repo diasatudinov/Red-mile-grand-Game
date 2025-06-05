7d66ada0578205f1c96cf61c4415970dc292a8f0
Dias <dias.atudinov@gmail.com>
2025-06-05 18:22:26 +0500

    ref

--- Red Mile Grand Game/Red Mile Grand Game/RMViews/RMGMiniGames/RMGFindCouple/MGMatchTheCardView.swift
+++ Red Mile Grand Game/Red Mile Grand Game/RMViews/RMGMiniGames/RMGFindCouple/MGMatchTheCardView.swift
@@ -1,11 +1,3 @@
-//
-//  MGMatchTheCardView.swift
-//  Red Mile Grand Game
-//
-//  Created by Dias Atudinov on 05.06.2025.
-//
-
-
 import SwiftUI
 
 struct MGMatchTheCardView: View {
--- Red Mile Grand Game/Red Mile Grand Game/RMViews/RMGMiniGames/RMGMazeGame/MGMazeViewContainer.swift
+++ Red Mile Grand Game/Red Mile Grand Game/RMViews/RMGMiniGames/RMGMazeGame/MGMazeViewContainer.swift
@@ -1,11 +1,3 @@
-//
-//  MGMazeViewContainer.swift
-//  Red Mile Grand Game
-//
-//  Created by Dias Atudinov on 05.06.2025.
-//
-
-
 import SwiftUI
 import SpriteKit
 
